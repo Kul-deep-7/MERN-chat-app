9e21e485f1aae0dab7fd8802ba17b64ec4d3e4c2
Kul-deep-7 <sahiljangid321@gmail.com>
2026-04-02 22:42:56 +0530

    imporved Leftsidebar css

--- frontend/src/components/LeftsideBar.jsx
+++ frontend/src/components/LeftsideBar.jsx
@@ -7,14 +7,17 @@ function LeftsideBar({selectedUser, setSelectedUser}) {
   return (
     <div>
         <div className='pb-5'>
-            <div className='flex justify-between item-center'>
+            <div className='flex justify-between items-center'>
                 <img src={assets.logo} alt='logo' className='max-w-40 ' />
-                <div className='relative py-2 group'>  {/*we are wrapping  the menu icon & the dropdown menu in a div and applying the group class to it(group is a Tailwind helper that lets parent control child styling. means When I get hovered (current parent div) my children(divs inside me) can change too). This allows us to show the dropdown menu when the user hovers over the menu icon */}
+                <div className='relative py-2 group'>  {/*we are wrapping  the menu icon & the dropdown menu(child) in a div(parent) and applying the group class to it(group is a Tailwind helper that lets parent control child styling. means When I get 
+                hovered (current parent div) my children(divs inside me) can change too). This allows us to show the dropdown menu when the user hovers over the menu icon cuz we are technically also hovering on parent div
+ */}
                     <img src={assets.menu_icon} alt='Menu' className='max-w-5 cursor-pointer' />
                     <div className='absolute top-full right-0 z-20 w-32 p-5 rounded-md
                     bg-[#282142] border border-gray-600 text-gray-100 hidden
                     group-hover:block'> {/*parent div is relative & this dropdown div is absolue means "Position relative to parent" So dropdown appears under menu icon, not somewhere random. & hidden here means the dropdown is hidden but 
-                                           when we hover on image the dropdown appears we added group-hover:block here so when we hover one image dropdown appears & stays but if we add the css to img tag the dropdown will go away resulting in bad UI */}
+                                           when we hover on img (we are technically also hovering on parent div) the dropdown appears we added group-hover:block here so when we hover one image dropdown appears & stays but if we add the css to img tag 
+                                           the dropdown will go away resulting in bad UI. group-hover:block means: When parent (group) is hovered → make this element visible (display: block)*/}
                         <p onClick={()=>navigate('/profile')}
                             className='cursor-pointer text-sm'>Edit Profile</p>
                         <hr className='my-2 border-t border-gray-500' />
