6ab8c4c49c2e44245c932156a4bdd7fc686751ba
Kul-deep-7 <sahiljangid321@gmail.com>
2026-04-12 08:32:34 +0530

    UI completed

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -9,11 +9,17 @@ const Profile = () => {
   const[name, setName] = useState("")
   const[bio, setBio] = useState("Hi Everyone, I am using ChatApp")
 
+  const handleSubmit=  async function (e) {
+    e.preventDefault()
+    navigate('/')
+  }
+
   return (
     <div className='min-h-screen bg-cover bg-no-repeat flex items-center justify-center'>
       <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 
       border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg'>
-        <form className='flex flex-col gap-5 p-10 flex-1'>
+        <form onSubmit={handleSubmit}
+          className='flex flex-col gap-5 p-10 flex-1'>
           <h3 className='text-lg'>Profile Details</h3>
           <label htmlFor="avatar" className='flex item-center gap-3 cursor-pointer'>
             <input onChange={(e)=>setSelectedImg(e.target.files[0])}
